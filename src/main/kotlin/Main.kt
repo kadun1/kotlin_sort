f0aeb765c86e76cd94f4bf6001eacc972157fa01
kadun1 <kadunjj@gmail.com>
2024-04-06 01:00:15 +0900

    [kotlin_sort] insert sort

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
     val array = arrayOf(5, 4, 3, 2, 1)
     bubbleSort(array)
+    insertSort(array)
 }
 
 fun bubbleSort(array: Array<Int>) {
@@ -12,5 +13,17 @@ fun bubbleSort(array: Array<Int>) {
                 array[j + 1] = temp
             }
         }
+    }
+}
+
+fun insertSort(array: Array<Int>) {
+    for(i in 1 until array.size){
+        var index = i
+        var temp = array[i]
+        while (index>0 && array[index-1]>temp){
+            array[index] = array[index-1]
+            index--
+        }
+        array[index]= temp
     }
 }
